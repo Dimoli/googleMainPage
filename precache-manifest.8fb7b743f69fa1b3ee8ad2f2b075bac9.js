49029d76f15d9a6eec94e3867f0792e42beff6c5
Dimoli <olifdmni@gmail.com>
2020-06-13 02:33:55 +0100

    Updates

---
+++ precache-manifest.8fb7b743f69fa1b3ee8ad2f2b075bac9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1cee678e27cd7785904af9f6019e6d",
+    "url": "/googleMainPage/index.html"
+  },
+  {
+    "revision": "930f88288c269e4d819f",
+    "url": "/googleMainPage/static/css/2.d84f2ad2.chunk.css"
+  },
+  {
+    "revision": "71868b644f430ecc80cd",
+    "url": "/googleMainPage/static/css/main.fe140d62.chunk.css"
+  },
+  {
+    "revision": "930f88288c269e4d819f",
+    "url": "/googleMainPage/static/js/2.55f79e12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/googleMainPage/static/js/2.55f79e12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71868b644f430ecc80cd",
+    "url": "/googleMainPage/static/js/main.81bb9775.chunk.js"
+  },
+  {
+    "revision": "8f27d430550567713d0e",
+    "url": "/googleMainPage/static/js/runtime-main.bf9690b9.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/googleMainPage/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/googleMainPage/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/googleMainPage/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/googleMainPage/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/googleMainPage/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
